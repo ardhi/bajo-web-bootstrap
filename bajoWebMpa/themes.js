13ea4cf3a0abd320b9088152d4348cbc482f3fd1
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-12 20:34:47 +0700

    Updates

--- bajoWebMpa/themes.js
+++ bajoWebMpa/themes.js
@@ -1,17 +1,25 @@
 async function themes () {
-  const { getConfig, readJson } = this.bajo.helper
-  const cfg = getConfig('bajoWebMpaBootstrap', { full: true })
-  const cfgStat = getConfig('bajoWebStatic', { full: true })
-  const virts = readJson(`${cfg.dir}/bajoWebStatic/virtuals.json`)
-  return {
-    name: 'bootstrap',
+  const { virtualsDir } = this.bajoWebStatic.helper
+  return [{
+    name: 'bootstrap5',
     description: 'Bootstrap 5.3',
-    script: `/${cfgStat.prefix}/${cfgStat.virtualsPrefix}/${cfg.alias}/${virts[0].prefix}/js/bootstrap.bundle.min.js`,
+    script: [
+      `${virtualsDir('bajoWebMpaBootstrap')}/bs5/js/bootstrap.bundle.min.js`
+    ],
     css: [
-      `/${cfgStat.prefix}/${cfgStat.virtualsPrefix}/${cfg.alias}/${virts[0].prefix}/css/bootstrap.min.css`,
-      `/${cfgStat.prefix}/${cfgStat.virtualsPrefix}/${cfg.alias}/${virts[1].prefix}/font/bootstrap-icons.min.css`
+      `${virtualsDir('bajoWebMpaBootstrap')}/bs5/css/bootstrap.min.css`
     ]
-  }
+  }, {
+    name: 'bootstrap4',
+    description: 'Bootstrap 4.6',
+    script: [
+      `${virtualsDir('bajoWebMpa')}/jquery/jquery.min.js`,
+      `${virtualsDir('bajoWebMpaBootstrap')}/bs4/js/bootstrap.bundle.min.js`
+    ],
+    css: [
+      `${virtualsDir('bajoWebMpaBootstrap')}/bs4/css/bootstrap.min.css`
+    ]
+  }]
 }
 
 export default themes
