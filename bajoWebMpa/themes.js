362541635d35763b9a546423bae47a1b2ad570e6
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-19 10:05:47 +0700

    Helper renamed

--- bajoWebMpa/themes.js
+++ bajoWebMpa/themes.js
@@ -1,23 +1,23 @@
 async function themes () {
-  const { virtualsDir } = this.bajoWebStatic.helper
+  const { virtualDir } = this.bajoWebStatic.helper
   return [{
     name: 'bootstrap5',
     description: 'Bootstrap 5.3',
     script: [
-      `${virtualsDir('bajoWebMpaBootstrap')}/bs5/js/bootstrap.bundle.min.js`
+      `${virtualDir('bajoWebMpaBootstrap')}/bs5/js/bootstrap.bundle.min.js`
     ],
     css: [
-      `${virtualsDir('bajoWebMpaBootstrap')}/bs5/css/bootstrap.min.css`
+      `${virtualDir('bajoWebMpaBootstrap')}/bs5/css/bootstrap.min.css`
     ]
   }, {
     name: 'bootstrap4',
     description: 'Bootstrap 4.6',
     script: [
-      `${virtualsDir('bajoWebMpa')}/jquery/jquery.min.js`,
-      `${virtualsDir('bajoWebMpaBootstrap')}/bs4/js/bootstrap.bundle.min.js`
+      `${virtualDir('bajoWebMpa')}/jquery/jquery.min.js`,
+      `${virtualDir('bajoWebMpaBootstrap')}/bs4/js/bootstrap.bundle.min.js`
     ],
     css: [
-      `${virtualsDir('bajoWebMpaBootstrap')}/bs4/css/bootstrap.min.css`
+      `${virtualDir('bajoWebMpaBootstrap')}/bs4/css/bootstrap.min.css`
     ]
   }]
 }
