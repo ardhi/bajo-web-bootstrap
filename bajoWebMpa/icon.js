94c4be1acfb8844cbddb791c686f03fc477effdb
Ardhi Lukianto <ardhi@lukianto.com>
2023-09-05 20:16:16 +0700

    New tags

---
+++ bajoWebMpa/icon.js
@@ -0,0 +1,8 @@
+const icon = {
+  directory: 'bi bi-folder2',
+  file: 'bi bi-file-earmark-text',
+  link: 'bi bi-link',
+  linkOut: 'bi bi-box-arrow-up-left'
+}
+
+export default icon
--- bajoWebMpa/themes.js
+++ bajoWebMpa/themes.js
@@ -1,3 +1,5 @@
+import icon from './icon.js'
+
 async function themes () {
   const { virtualDir } = this.bajoWebStatic.helper
   return [{
@@ -7,8 +9,10 @@ async function themes () {
       `${virtualDir('bajoWebMpaBootstrap')}/bs5/js/bootstrap.bundle.min.js`
     ],
     css: [
-      `${virtualDir('bajoWebMpaBootstrap')}/bs5/css/bootstrap.min.css`
+      `${virtualDir('bajoWebMpaBootstrap')}/bs5/css/bootstrap.min.css`,
+      `${virtualDir('bajoWebMpaBootstrap')}/icons/font/bootstrap-icons.min.css`
     ],
+    icon,
     mapping: {
       sizing: {
         small: 'sm',
@@ -29,8 +33,10 @@ async function themes () {
       `${virtualDir('bajoWebMpaBootstrap')}/bs4/js/bootstrap.bundle.min.js`
     ],
     css: [
-      `${virtualDir('bajoWebMpaBootstrap')}/bs4/css/bootstrap.min.css`
+      `${virtualDir('bajoWebMpaBootstrap')}/bs4/css/bootstrap.min.css`,
+      `${virtualDir('bajoWebMpaBootstrap')}/icons/font/bootstrap-icons.min.css`
     ],
+    icon,
     mapping: {
       sizing: {
         small: 'sm',
